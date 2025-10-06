7ca4c34a501d2925960637cce2a0b8950c3176f5
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-06 21:54:56 +0300

    configuring the image uploader to django

--- amen_furniture/settings.py
+++ amen_furniture/settings.py
@@ -156,3 +156,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
 }
+#for adding images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
