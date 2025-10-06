2e266d9fcfce6d3946fd85fac7e8d8d09ed64c34
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-06 12:27:49 +0300

    configuring jwt

--- amen_furniture/settings.py
+++ amen_furniture/settings.py
@@ -141,3 +141,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+}
