acf8523914eb364ae725fca0afd2b035c3c66977
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-06 14:32:20 +0300

    creating a registration view

--- amen_furniture/settings.py
+++ amen_furniture/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #third-party apps
     'rest_framework',
+    'rest_framework_simplejwt',
 
 
     #apps
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics,permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer,UserSerializer
+from .models import User
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+    
