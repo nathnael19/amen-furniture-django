60a2483de9ef0af303f5c19ea000d3904734919f
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-06 12:30:55 +0300

    creating register serializer

--- users/serializers.py
+++ users/serializers.py
@@ -15,3 +15,23 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
         return user
+    
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['username','email','phone','password','first_name','last_name']
+
+    
+    def create(self,validated_data):
+        return User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            phone=validated_data['phone'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+        )
+    
