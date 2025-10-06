97c1fb8d1eccdfe18fcbc08b6b5cc9c9797d4156
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-06 11:49:54 +0300

    creating a user model:

--- amen_furniture/settings.py
+++ amen_furniture/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third-party apps
+    'rest_framework',
+
+
+    #apps
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True,)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    phone = models.CharField(max_length=10,unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','phone','first_name','last_name']
+
+    def __str__(self):
+        return self.username
