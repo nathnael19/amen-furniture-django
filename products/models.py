bcb5b74ab12788f7005042af282ea7430bf82dc2
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-06 21:27:31 +0300

    creating a product model

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.conf import settings
 import uuid
 
 # Create your models here.
@@ -20,5 +21,35 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
+    name = models.CharField(max_length=250)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    slug = models.SlugField(unique=True,blank=True)
+    description = models.TextField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    is_active = models.BooleanField(default=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name','category']
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+
 
 
