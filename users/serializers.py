3bced975a286c060a81358147ae586ee59385962
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-06 12:32:25 +0300

    creating a login serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -34,4 +35,19 @@ class RegisterSerializer(serializers.ModelSerializer):
             first_name = validated_data['first_name'],
             last_name = validated_data['last_name'],
         )
-    
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+
+        user = authenticate(email=email,password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+        data['user'] = user
+        return data
