287258463619296c193580203d536a86e03879fb
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-08 11:08:26 +0300

    creating a url pattern for the views

--- amen_furniture/urls.py
+++ amen_furniture/urls.py
@@ -6,7 +6,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/',include('users.urls')),
-    path('api/v1/',include('products.urls'))
+    path('api/v1/',include('products.urls')),
+    path('api/v1/cart/',include('cart.urls')),
 ]
 
 if settings.DEBUG:
