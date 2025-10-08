b74a57fc271db483b2eee08d3545a105ce5bc734
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-08 10:23:35 +0300

    creating a cart item model

--- cart/models.py
+++ cart/models.py
@@ -10,9 +10,25 @@ class Cart(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
+    @property
     def total(self):
         return sum(item.subtotal for item in self.items.all())
     
     def __str__(self):
         return f'Cart ({self.user or "Guest"})'
+    
+
+class CartItem(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name='items')
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    addedAt = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def subtotal(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return f'{self.product.name} x {self.quantity}'
 
